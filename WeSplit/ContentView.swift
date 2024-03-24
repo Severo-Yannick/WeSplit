ee825dc6184611a54e9f9c52f911a0393e875c19
Yannick SEVERO <ysevero@squad.fr>
2024-03-24 14:31:32 +0100

    Initial commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipePercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    let tipePercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let typeSelection = Double(tipePercentage)
+        
+        let typeValue = amount / 100 * typeSelection
+        let grandTotal = amount + typeValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section{
+                    TextField("Enter your amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("How much do you want to tipe ?") {
+                    Picker("Tipe Percentages", selection: $tipePercentage) {
+                        ForEach(tipePercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                }
+            }
+            .navigationTitle("WeSplit").opacity(0.8)
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
-#Preview {
-    ContentView()
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+#endif
